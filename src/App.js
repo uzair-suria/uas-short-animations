434e626d074e00758c6deadcd154d3f2a321e3f9
Uzair Suria <uzair.a.suria@outlook.com>
2020-12-17 00:13:31 +0500

    Added Blob component for testing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import Blob from './components/Blob';
+
 function App() {
 	return (
 		<div className="App" style={{ textAlign: 'center' }}>
 			<h4>Website under development...</h4>
 			<div>Please Visit Later</div>
+			<Blob>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur omnis
+				provident exercitationem illo ratione, officiis natus ad. Iure dolorem
+				quisquam beatae laboriosam at qui earum voluptatibus vel accusamus
+				voluptas? Inventore.
+			</Blob>
 		</div>
 	);
 }
