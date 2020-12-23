0c773a4317e780101c36e6a33d29e7d7d4ebb736
Uzair Suria <uzair.a.suria@outlook.com>
2020-12-23 22:14:31 +0500

    Create animate-components.js to export the animated components

---
+++ src/components/animated-components.js
@@ -0,0 +1,7 @@
+export { default as AnimatedBlob } from './AnimatedBlob';
+
+export { default as AnimatedCollaboration } from './AnimatedCollaboration';
+
+export { default as AnimatedLogo } from './AnimatedLogo';
+
+export { default as AnimatedWebsite } from './AnimatedWebsite';
